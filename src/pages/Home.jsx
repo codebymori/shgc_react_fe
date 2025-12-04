8eab3300d1f444dd7b1e618c8e2796c8a43f352f
admin <admin@192.168.1.14>
2025-12-04 17:04:15 +0700

    routing

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="bg-black h-[100vh] justify-center flex items-center text-white text-[50px]">
+      <p>bismillah desember kelar</p>
+    </div>
+  );
+};
+
+export default Home;
