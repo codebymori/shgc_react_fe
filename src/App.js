dbd5dddfd703638223f487013b90c455c4ca680f
djorgi <djorgi@192.168.1.13>
2025-12-04 17:16:59 +0700

    solved

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
 
 function App() {
   return (
-    <div className="bg-black h-[100vh] justify-center flex items-center text-white text-[50px]">
-      <p>bismillah desember kelar</p>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
